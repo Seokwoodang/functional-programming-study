a21e428e781ece127022526c705b90e574014f1a
Seokwoodang <swoo1429@naver.com>
2024-10-04 20:57:29 +0900

    add : 섹션4. reduce 실습완료

--- 04.map, filter, reduce/section4.js
+++ 04.map, filter, reduce/section4.js
@@ -69,3 +69,40 @@ console.log(
     })()
   )
 ); // [1, 3, 5]
+
+// reduce
+
+const nums = [1, 2, 3, 4, 5];
+
+let total = 0;
+for (const n of nums) {
+  total = total + n;
+}
+log(total);
+
+const reduce = (f, acc, iter) => {
+  if (!iter) {
+    iter = acc[Symbol.iterator]();
+    // 1번째 값으로 초기화
+    acc = iter.next().value;
+  }
+  for (const a of iter) {
+    acc = f(acc, a);
+  }
+  return acc;
+};
+
+const add = (a, b) => a + b;
+
+log(reduce(add, 0, [1, 2, 3, 4, 5]));
+// 15
+
+log(add(add(add(add(add(0, 1), 2), 3), 4), 5));
+// 15
+
+log(reduce(add, [1, 2, 3, 4, 5]));
+// 15
+
+// console.clear();
+
+reduce((total_price, product) => total_price + product.price, 0, products);
