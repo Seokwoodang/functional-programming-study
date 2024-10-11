db1f515565796b4be11c4abb5145fd674791b160
Seokwoodang <swoo1429@naver.com>
2024-10-11 15:19:14 +0900

    add : 섹션7. take 실습완료

--- 07. 지연성 1/section7.js
+++ 07. 지연성 1/section7.js
@@ -30,14 +30,37 @@ let add = (a, b) => a + b;
 // 느긋한 L.range
 // generator로 생성된 함수로 순회를 시작하는 next()가 실행되어야 결과가 꺼내진다.
 
-const L = {};
-L.range = function* (l) {
-  let i = -1;
-  while (++i < l) {
-    yield i;
-  }
-};
+// const L = {};
+// L.range = function* (l) {
+//   let i = -1;
+//   while (++i < l) {
+//     yield i;
+//   }
+// };
 
-var list = L.range(4);
-console.log(list);
-console.log(reduce(add, list));
+// var list = L.range(4);
+// console.log(list);
+// console.log(reduce(add, list));
+
+// ## take
+
+const curry =
+  (f) =>
+  (a, ..._) =>
+    _.length ? f(a, ..._) : (..._) => f(a, ..._);
+
+const take = curry((l, iter) => {
+  let res = [];
+  for (const a of iter) {
+    res.push(a);
+    if (res.length == l) return res;
+  }
+  return res;
+});
+console.time("");
+go(range(10000), take(5), reduce(add), log);
+console.timeEnd("");
+
+console.time("");
+go(L.range(10000), take(5), reduce(add), log);
+console.timeEnd("");
