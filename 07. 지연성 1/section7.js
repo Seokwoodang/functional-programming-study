91af3e59d8b913a97184f94f73a053da9a0f35af
Seokwoodang <swoo1429@naver.com>
2024-10-11 16:32:50 +0900

    add : 섹션7. L.range, L.map, L.filter, L.take, L.reduce 중첩 실습완료

--- 07. 지연성 1/section7.js
+++ 07. 지연성 1/section7.js
@@ -124,3 +124,32 @@ const reduce = curry((f, acc, iter) => {
   }
   return acc;
 });
+
+// ### L.range, L.map, L.filter, take, reduce 중첩 사용
+
+L.range = function* (l) {
+  let i = -1;
+  while (++i < l) {
+    yield i;
+  }
+};
+
+L.map = curry(function* (f, iter) {
+  iter = iter[Symbol.iterator]();
+  let cur;
+  while (!(cur = iter.next()).done) {
+    const a = cur.value;
+    yield f(a);
+  }
+});
+
+L.filter = curry(function* (f, iter) {
+  iter = iter[Symbol.iterator]();
+  let cur;
+  while (!(cur = iter.next()).done) {
+    const a = cur.value;
+    if (f(a)) {
+      yield a;
+    }
+  }
+});
